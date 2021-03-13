3d105493784cdd03f54c4c1de609f2e7bb85f595
alxl <doomcookiealex@gmail.com>
2021-03-13 14:11:34 -0600

    Move the reset function to sv

--- lua/autorun/server/sv_stoppower_ply_meta.lua
+++ lua/autorun/server/sv_stoppower_ply_meta.lua
@@ -4,6 +4,10 @@ function plymeta:SetStopPowerSlowdownMult(mult)
     self:SetNWFloat("stoppower_mult", mult)
 end
 
+function plymeta:ResetStopPowerMult()
+    self:SetNWFloat("stoppower_mult", 1.0)
+end
+
 function plymeta:ChangeStopPowerSlowdownMult(by, minAllowed)
     self:SetStopPowerSlowdownMult(math.min(1.0, math.max(minAllowed, self:GetStopPowerSlowdownMult() + by)))
 end
--- lua/autorun/sh_stoppower.lua
+++ lua/autorun/sh_stoppower.lua
@@ -4,10 +4,6 @@ function plymeta:GetStopPowerSlowdownMult()
     return self:GetNWFloat("stoppower_mult", 1.0)
 end
 
-function plymeta:ResetStopPowerMult()
-    self:SetNWFloat("stoppower_mult", 1.0)
-end
-
 function plymeta:GetRecoveryTime()
     return self:GetNWFloat("stoppower_recov_delay")
 end
