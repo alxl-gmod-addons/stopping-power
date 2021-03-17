a5527b75775c2b185520daa880783ef0ca3e6636
alxl <doomcookiealex@gmail.com>
2021-03-17 14:18:48 -0500

    Additional mults for specific damage types

--- lua/autorun/server/sv_stoppower.lua
+++ lua/autorun/server/sv_stoppower.lua
@@ -25,12 +25,6 @@ cvars.AddChangeCallback("stoppower_tick_time", function()
     tickTime = GetConVar("stoppower_tick_time"):GetFloat()
 end)
 
-CreateConVar("stoppower_dmg_mult", 0.05, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The slowdown amount per damage taken.")
-local dmgMult = GetConVar("stoppower_dmg_mult"):GetFloat()
-cvars.AddChangeCallback("stoppower_dmg_mult", function()
-    dmgMult = GetConVar("stoppower_dmg_mult"):GetFloat()
-end)
-
 CreateConVar("stoppower_minimum_speed_mult", 0.1, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The slowest slowdown amount allowed.")
 local multMin = GetConVar("stoppower_minimum_speed_mult"):GetFloat()
 cvars.AddChangeCallback("stoppower_minimum_speed_mult", function()
@@ -47,13 +41,6 @@ CreateConVar("stoppower_recovery_delay", 0.5, FCVAR_ARCHIVE + FCVAR_NOTIFY,
     "The number of seconds before recovery starts.")
 CreateConVar("stoppower_autoreset_type", 1, FCVAR_ARCHIVE + FCVAR_NOTIFY, "0=no auto-reset, 1=on spawn, 2=on death")
 
-hook.Add("PlayerHurt", "StoppingPowerSlowdown", function(ply, attacker, hpRemain, dmgTaken)
-    if enabled then
-        ply:ChangeStopPowerSlowdownMult(-dmgMult * dmgTaken, multMin)
-        ply:SetStopPowerRecoveryTime(CurTime() + GetConVar("stoppower_recovery_delay"):GetFloat())
-    end
-end)
-
 hook.Add("PostPlayerDeath", "StoppingPowerDeathReset", function(ply)
     if enabled and GetConVar("stoppower_recovery_delay"):GetInt() == 1 then
         ply:ResetStopPowerMult()
@@ -63,6 +50,63 @@ end)
 hook.Add("PlayerSpawn", "StoppingPowerSpawnReset", function(ply, transition)
     if enabled and GetConVar("stoppower_recovery_delay"):GetInt() == 2 then
         ply:ResetStopPowerMult()
+    end
+end)
+
+CreateConVar("stoppower_dmg_mult", 0.05, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The base slowdown amount per damage taken.")
+CreateConVar("stoppower_dmg_mult_armor", 0.8, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The multiplier for slowdown caused by damage taken when you have any armor.")
+CreateConVar("stoppower_dmg_mult_bullet", 1.0, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The multiplier for slowdown caused by conventional bullets.")
+CreateConVar("stoppower_dmg_mult_drown", 0.0, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The multiplier for slowdown caused by drowning damage.")
+CreateConVar("stoppower_dmg_mult_energy", 0.9, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The multiplier for slowdown caused by lasers, plasma, electricity, etc.")
+CreateConVar("stoppower_dmg_mult_explosion", 1.1, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The multiplier for slowdown caused by explosions.")
+CreateConVar("stoppower_dmg_mult_falling", 0.25, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The multiplier for slowdown caused by falling.")
+CreateConVar("stoppower_dmg_mult_fire", 0.9, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The multiplier for slowdown caused by fire.")
+CreateConVar("stoppower_dmg_mult_impact", 1.1, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The multiplier for slowdown caused by props and blunt melee attacks.")
+CreateConVar("stoppower_dmg_mult_poison", 0.1, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The multiplier for slowdown caused by poison (e.g. from poison headcrabs).")
+CreateConVar("stoppower_dmg_mult_slash", 1.0, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The multiplier for slowdown caused by slashes (includes almost all HL2 NPC melee attacks).")
+CreateConVar("stoppower_dmg_mult_toxin", 0.9, FCVAR_ARCHIVE + FCVAR_NOTIFY, "The multiplier for slowdown caused by acid, radiation, etc.")
+
+hook.Add("PostEntityTakeDamage", "StoppingPowerSlowdown", function(ent, dmg, took)
+    if enabled and took and ent:IsPlayer() then
+        local dmgMult = GetConVar("stoppower_dmg_mult"):GetFloat()
+        if ent:Armor() > 0 then
+            dmgMult = dmgMult * GetConVar("stoppower_dmg_mult_armor"):GetFloat()
+        end
+        if dmg:IsBulletDamage() then
+            dmgMult = dmgMult * GetConVar("stoppower_dmg_mult_bullet"):GetFloat()
+        end
+        if dmg:IsDamageType(DMG_DROWN) then
+            dmgMult = dmgMult * GetConVar("stoppower_dmg_mult_drown"):GetFloat()
+        end
+        if dmg:IsDamageType(DMG_SHOCK) or dmg:IsDamageType(DMG_SONIC) or dmg:IsDamageType(DMG_ENERGYBEAM) or dmg:IsDamageType(DMG_PLASMA)  or dmg:IsDamageType(DMG_PHYSGUN) then
+            dmgMult = dmgMult * GetConVar("stoppower_dmg_mult_energy"):GetFloat()
+        end
+        if dmg:IsExplosionDamage() then
+            dmgMult = dmgMult * GetConVar("stoppower_dmg_mult_explosion"):GetFloat()
+        end
+        if dmg:IsFallDamage() then
+            dmgMult = dmgMult * GetConVar("stoppower_dmg_mult_falling"):GetFloat()
+        end
+        if dmg:IsDamageType(DMG_BURN) or dmg:IsDamageType(DMG_SLOWBURN) then
+            dmgMult = dmgMult * GetConVar("stoppower_dmg_mult_fire"):GetFloat()
+        end
+        if dmg:IsDamageType(DMG_CLUB) or dmg:IsDamageType(DMG_CRUSH) or dmg:IsDamageType(DMG_GENERIC) or dmg:IsDamageType(DMG_VEHICLE) then
+            dmgMult = dmgMult * GetConVar("stoppower_dmg_mult_impact"):GetFloat()
+        end
+        if dmg:IsDamageType(DMG_POISON) or dmg:IsDamageType(DMG_PARALYZE) then
+            dmgMult = dmgMult * GetConVar("stoppower_dmg_mult_poison"):GetFloat()
+        end
+        if dmg:IsDamageType(DMG_SLASH) then
+            dmgMult = dmgMult * GetConVar("stoppower_dmg_mult_slash"):GetFloat()
+        end
+        if dmg:IsDamageType(DMG_NERVEGAS) or dmg:IsDamageType(DMG_RADIATION) or dmg:IsDamageType(DMG_ACID) then
+            dmgMult = dmgMult * GetConVar("stoppower_dmg_mult_toxin"):GetFloat()
+        end
+
+        if dmgMult > 0.0 then
+            ent:ChangeStopPowerSlowdownMult(-dmgMult * dmg:GetDamage(), multMin)
+            ent:SetStopPowerRecoveryTime(CurTime() + GetConVar("stoppower_recovery_delay"):GetFloat())
+        end
     end
 end)
 
